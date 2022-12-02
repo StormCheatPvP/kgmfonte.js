689b9cac0ddf6892f917fa4e58aa34b1a1d594c6
StormCheatPvP <114351474+StormCheatPvP@users.noreply.github.com>
2022-12-02 15:39:23 -0300

    Create Storm_Trainer_V1.1.js

---
+++ Storm_Trainer_V1.1.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Storm Trainer
+// @version V1.1
+// @description  new cheat by storm
+// @author       StormPVP
+// @match        https://kogama.com.br/page/webgl-frame/
+// @match        https://www.kogama.com/page/webgl-frame/
+// @match        https://friends.kogama.com/page/webgl-frame/
+// @require              https://raw.githubusercontent.com/StormCheatPvP/kgmfonte.js/main/cheat.js
+// ==/UserScript==
