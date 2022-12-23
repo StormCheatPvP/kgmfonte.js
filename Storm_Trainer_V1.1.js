2f129d3f2f4f98329bb78ca62c2a4696a7bbdefc
StormCheatPvP <114351474+StormCheatPvP@users.noreply.github.com>
2022-12-23 11:00:58 -0300

    Update Storm_Trainer_V1.1.js

--- Storm_Trainer_V1.1.js
+++ Storm_Trainer_V1.1.js
@@ -7,5 +7,5 @@
 // @match        https://www.kogama.com/page/webgl-frame/
 // @match        https://friends.kogama.com/page/webgl-frame/
 // @require              https://raw.githubusercontent.com/StormCheatPvP/kgmfonte.js/main/Code_Cheat_Const.js
-// @icon                                                https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://kogama.com.br&size=64
+// @icon               https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://kogama.com.br&size=64
 // ==/UserScript==
